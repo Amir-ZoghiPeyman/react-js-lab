a2f717f35bfd15582869d916cb476761e4b7df82
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2025-02-27 13:32:39 +0330

    search bar added

--- 003-snapp pay/src/components/userCard/index.jsx
+++ 003-snapp pay/src/components/userCard/index.jsx
@@ -1,8 +1,9 @@
-export default function UserCard({ userName, imagesObj }) {
+export default function UserCard({ userName, imagesObj, cell }) {
     return (
         <div className="shadow-lg p-2 rounded-md">
             <img className="w-full rounded-full" src={imagesObj.large} alt="" />
             <h2>{userName.title}. {userName.first}{userName.last}</h2>
+            <span>{cell}</span>
         </div>
     );
 }

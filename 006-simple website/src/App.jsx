e8e8f9f92329c2126d34492985493dd3941d4ace
Amir-ZoghiPeyman <zoghipeyman.amir@gmail.com>
2025-04-29 22:20:26 +0330

    new routes with new tech added

--- 006-simple website/src/App.jsx
+++ 006-simple website/src/App.jsx
@@ -1,4 +1,7 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CSSTransition } from "react-transition-group";
 import Home from "./pages/home"
 import About from "./pages/about";
 import Products from "./pages/product";
@@ -6,25 +9,30 @@ import Dashboard from "./pages/dashboard";
 import DashboardHome from "./pages/dashboardHome";
 import Stats from "./pages/stats";
 import NotFound from "./pages/notFound";
+import ProtectedRoute from "./components/protectedRoute";
 
 
 export default function App() {
+  const queryClient = new QueryClient();
+  const Product = lazy(() => import("./components/product"))
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/product/:id" element={<Products />} />
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route index element={<DashboardHome />} />
-            <Route path="stats" element={<Stats />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      < QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<CSSTransition in={true} timeout={300} classNames="fade" unmountOnExit><About /></CSSTransition>} />
+            <Route path="/product" element={<Suspense fallback={<div>2</div>}><Product /></Suspense>} />
+            <Route path="/product/:id" element={<Products />} />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>}>
+              <Route index element={<DashboardHome />} />
+              <Route path="stats" element={<Stats />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   )
 }
-
--- 006-simple website/src/pages/product/index.jsx
+++ 006-simple website/src/pages/product/index.jsx
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export default function Products() {
     const { id } = useParams();
+    const { search, setSearch } = useSearchParams();
+
+    const productSearch = search.get('search') || 'all';
+    const handleProduct = (pro) => (
+        setSearch({ productSearch: pro })
+    )
+
+
     return (
         <>
+            <button onClick={() => handleProduct('all')}>All</button>
             <h1>product {id}</h1>
         </>
     )
